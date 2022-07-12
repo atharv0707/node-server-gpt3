3f09d8d6b99f5ba6d4098762bf72f41ffef5cfe5
Atharv Tiwari <atharv@Shirishs-MacBook-Pro.local>
2022-07-12 16:23:06 +0100

    bug fix

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ app.use(express.json());
 app.post('/', async (req, res) => {
     const { code, newLanguageType, oldLanguageType } = req.body;
     const translate = await getTranslatedCode(code, newLanguageType, oldLanguageType);
-    res.json(
-        { translation: translate, message: 'translation success'}); }
-})
+    res.json({ translation: translate, message: 'translation success'}); })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
